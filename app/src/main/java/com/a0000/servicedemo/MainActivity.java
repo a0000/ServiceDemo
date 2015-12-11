931f27fe1ef3f0598ff87a99636ac8ae5d2dfa6d
Administrator <Administrator>
2015-12-11 19:14:15 +0800

    update

--- app/src/main/java/com/a0000/servicedemo/MainActivity.java
+++ app/src/main/java/com/a0000/servicedemo/MainActivity.java
@@ -9,13 +9,20 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
     /** 参数设置 */
@@ -23,10 +30,25 @@ public class MainActivity extends Activity {
     private PendingIntent pendingIntent;
     private final int HEARTBEAT_INTERVAL = 2 * 1000;
 
+    private long start = System.currentTimeMillis();
+    ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+//        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "TAG");
+//        wakeLock.acquire();
+//
+//        exec.scheduleAtFixedRate(new Runnable() {
+//            @Override
+//            public void run() {
+//                String format = String.format("----Activity--------  %7d --  %s", (System.currentTimeMillis() - start), new Date().toLocaleString());
+//                System.out.println(format);
+//                start = System.currentTimeMillis();
+//                MyService.write2SDCard("activity.txt", format);
+//            }
+//        }, 1, 12, TimeUnit.SECONDS);
     }
 
     public void onstart(View v) {
@@ -93,5 +115,14 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         System.out.println("=======onDestroy=======");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);//true表示在任何一个activity下都可以退出，false表示只能在根activity下退出
+            return true;//true则表示监听被跳出，不再执行其他监听事件了
+        }
+        return false;
     }
 }
--- app/src/main/java/com/a0000/servicedemo/MyService.java
+++ app/src/main/java/com/a0000/servicedemo/MyService.java
@@ -1,11 +1,24 @@
 package com.a0000.servicedemo;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
+import android.os.PowerManager;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MyService extends Service {
+    ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
     public MyService() {
     }
 
@@ -20,14 +33,53 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        System.out.println("----onStartCommand-------- " + (System.currentTimeMillis() - start));
-        start = System.currentTimeMillis();
+//        String format = String.format("----onStartCommand--------  %7d --  %s", (System.currentTimeMillis() - start), new Date().toLocaleString());
+//        System.out.println(format);
+//        start = System.currentTimeMillis();
+        //write2SDCard(format);
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "TAG");
+        wakeLock.acquire();
+
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                String format = String.format("----onStartCommand--------  %7d --  %s", (System.currentTimeMillis() - start), new Date().toLocaleString());
+                System.out.println(format);
+                start = System.currentTimeMillis();
+                write2SDCard("service.txt", format);
+            }
+        }, 1, 12, TimeUnit.SECONDS);
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    public static void write2SDCard(String fileName, String s) {
+        try {
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                String sdCard = Environment.getExternalStorageDirectory().getPath();
+                String path = sdCard+"/小组饭/";
+                File dir = new File(path);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                path += fileName;
+                System.out.println("---------- " + path);
+                FileWriter fw = new FileWriter(path, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.append(s);
+                bw.newLine();
+                bw.close();
+                fw.close();
+            }
+        } catch (Exception e) {
+            System.out.println("------------ " +e.getMessage());
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        exec.shutdownNow();
         System.out.println("----onDestroy--------");
     }
 
